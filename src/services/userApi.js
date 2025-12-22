4a4a20ed04d60dd79adb76d7fa8bf87b1d6e385d
SriHaran23 <sriharankandepalli@gmail.com>
2025-12-22 13:18:03 +0530

    api_base_url

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,11 +1,16 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const API_BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:4500"
+    : window.location.origin;
+
 // Define a service using a base URL and expected endpoints
 export const usersApi = createApi({
   reducerPath: "usersApi",
-  // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4500/api/users" }),
-  baseQuery: fetchBaseQuery({ baseUrl: "https://vakalath-backend-production.up.railway.app/api/users" }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${API_BASE_URL}/api/users` }),
+  // baseQuery: fetchBaseQuery({ baseUrl: "https://vakalath-backend-production.up.railway.app/api/users" }),
   endpoints: (builder) => ({
     signUp: builder.mutation({
       query: (userData) => ({
